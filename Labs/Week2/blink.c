2c20bef18e087d31e3a09a577df277c7d66a70a8
andrew_collison <andrew.collison.ac@gmail.com>
2018-03-01 13:20:15 +1000

    more flashing lights

--- Labs/Week2/blink.c
+++ Labs/Week2/blink.c
@@ -3,9 +3,18 @@
 #define F_CPU 16000000UL // Set Clock Speed
 
 #include <avr/io.h>
-
+#include <avr/delay.h>
 
 void main(){
-	DDRB |= (1<<1);
-	PORTB |= (1<<1);
+	while(1){
+		DDRB |= (1<<1)|(1<<2);
+		PORTB ^= (1<<1);
+		_delay_ms(500);
+		PORTB ^= (1<<2);
+		_delay_ms(500);
+		// _delay_ms(1000);
+		// PORTB &= (0<<1);
+		// _delay_ms(1000);
+	}
+	
 }
